6c67b708c65691e161af60f2897bbf3ce59db884
Eduardo Parra <eduparra90@gmail.com>
2026-05-21 18:55:45 +0200

    fix

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,8 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://soker90.github.io",
-  base: "/tarifas-luz",
+  site: "https://tarifas-luz.vercel.app",
   vite: {
     plugins: [tailwindcss()],
   },
